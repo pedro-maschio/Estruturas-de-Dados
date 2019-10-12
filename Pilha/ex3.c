88800234f9c8b5593ada44be51bc3474ed54e86d
Pedro de Tôrres maschio <pedrotorres_maschio@hotmail.com>
2019-10-12 17:32:07 -0300

    Envia

--- Pilha/ex3.c
+++ Pilha/ex3.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+typedef struct {
+    int topo;
+    int anilhas[1001];
+} Pilha;
+
+void criaPilha(Pilha *pilha) {
+    pilha->topo = -1;
+}
+void empilha(Pilha *pilha, int peso) {
+    pilha->anilhas[++pilha->topo] = peso;
+}
+void retirar(Pilha *pilha, int retira) {
+    int pesoTotal = 0, tam = pilha->topo;
+
+    while(pilha->anilhas[tam] >= 0 && pilha->anilhas[tam] != retira) {
+        printf("Peso retirado: %d\n", pilha->anilhas[tam]);
+        pesoTotal += pilha->anilhas[tam];
+        tam--;
+    }
+    printf("Anilhas retiradas: %d\n", pilha->topo-tam);
+    printf("Peso retirado: %d\n", pesoTotal);
+}
+
 int main() {
+    Pilha *pilha = malloc(sizeof(Pilha));
+    criaPilha(pilha);
+    int d, retira;
+
+    while(1) {
+        scanf("%d", &d);
+        if(d == 0)
+            break;
+        empilha(pilha, d);
+
+    }
+    scanf("%d", &retira);
+    retirar(pilha, retira);
 
     return 0;
 }
